c2b36fcf4ef7e841f2fdabe99237caa15a96a9ee
Tyler <44188028+twit96@users.noreply.github.com>
2022-09-27 12:20:16 -0500

    Moved JavaScript into its own file
    To help organize the HTML, inline scripts are now moved into their own external script file, named 'main.js'. File is called from a tag in the HTML head.

---
+++ main.js
@@ -0,0 +1,46 @@
+/*
+
+Table of Contents
+
+- Background Pattern
+- Go to Top Button
+
+*/
+
+
+// Background Pattern ---------------------------------------------------------
+// add random squares to background element
+var bg = document.getElementById("bg");
+var rand_span;
+var rand_x;
+var rand_y;
+var rand_size;
+var rand_rotation;
+for (i=0; i<30; i++) {
+  rand_span = document.createElement("span");
+  // generate random attributes
+  rand_x = Math.floor(Math.random() * 100);         // 0 to 100 vw
+  rand_y = Math.floor(Math.random() * 100);         // 0 to 100 vh
+  rand_size = Math.floor(Math.random() * 15) + 10;  // 10 to 25 vmin
+  rand_rotation = Math.floor(Math.random() * 360);  // 0 to 360 deg
+  // set CSS styles
+  rand_span.style.left = rand_x + "vw";
+  rand_span.style.top = rand_y + "vh";
+  rand_span.style.width = rand_size + "vmin";
+  rand_span.style.height = rand_size + "vmin";
+  rand_span.style.transform = "rotate(" + rand_rotation + "deg)";
+  // place in bg
+  bg.insertAdjacentElement("beforeend", rand_span);
+}
+
+
+// Go to Top Button -----------------------------------------------------------
+// toggle gototop visibility based on page scroll position
+var gototop = document.getElementById("gototop");
+window.onscroll = function() {
+  if (
+    (document.body.scrollTop > 250) ||
+    (document.documentElement.scrollTop > 250)
+  ) { gototop.style.display = "block"; }
+  else { gototop.style.display = "none"; }
+};
