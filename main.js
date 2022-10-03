d863f526ff5c6f52c636c963bc72c765b9917e68
Tyler <44188028+twit96@users.noreply.github.com>
2022-10-03 17:02:03 -0500

    Re-Overhauled Highlights Section
    Section is now an interactive carousel, appearing at the top of the page on mobile, and above the sidebar on desktop.

--- main.js
+++ main.js
@@ -43,3 +43,126 @@ window.onscroll = function() {
   ) { gototop.style.display = "block"; }
   else { gototop.style.display = "none"; }
 }
+
+
+// Highlights Carousel --------------------------------------------------------
+let highlights_section = document.getElementById("highlights");
+let highlights = highlights_section.getElementsByClassName("highlight");
+let num_cards = highlights.length;
+
+// Pause Animation on Mouse Hover
+let hovering = false;
+highlights_section.onmouseover = function() {
+  hovering = true;
+}
+highlights_section.onmouseout = function() {
+  hovering = false;
+  resetTimer();
+}
+
+// Automatically Cycle Between Cards
+let curr = 0;
+let prev = (curr - 1 + num_cards) % num_cards;
+let next = (curr + 1) % num_cards;
+
+// configure the required initial card classes
+function configClasses() {
+  for (let i=0; i<num_cards; i++) {
+    highlights[i].classList.add('no-anim');
+    highlights[i].classList.add('shift-left');
+  }
+
+  highlights[curr].classList.remove('shift-left');
+  highlights[curr].classList.remove('no-anim');
+
+  highlights[next].classList.add('no-anim');
+  highlights[next].classList.add('shift-right');
+}
+configClasses();
+
+// update the classes for the appropriate cards at each iteration
+function incrementClasses() {
+  highlights[prev].classList.add('shift-left');
+
+  highlights[curr].classList.remove('no-anim');
+  highlights[curr].classList.remove('shift-right');
+
+  highlights[next].classList.add('no-anim');
+  highlights[next].classList.remove('shift-left');
+  highlights[next].classList.add('shift-right');
+}
+incrementClasses();  // first iteration
+
+function decrementClasses() {
+  highlights[prev].classList.add('no-anim');
+  highlights[prev].classList.remove('shift-right');
+  highlights[prev].classList.add('shift-left');
+
+  highlights[curr].classList.remove('no-anim');
+  highlights[curr].classList.remove('shift-left');
+
+  highlights[next].classList.add('shift-right');
+}
+
+// keep track of which cards to update at each iteration
+function incrementIndeces() {
+  curr = (curr + 1) % num_cards;
+  prev = (curr - 1 + num_cards) % num_cards;
+  next = (curr + 1) % num_cards;
+}
+
+function decrementIndeces() {
+  curr = (curr - 1 + num_cards) % num_cards;
+  prev = (curr - 1 + num_cards) % num_cards;
+  next = (curr + 1) % num_cards;
+}
+
+// Visual Counter
+let counter = document.getElementById("highlights-counter");
+
+// update counter at each iteration
+function updateCounter() {
+  counter.innerHTML = curr+1 + ' / ' + num_cards;
+}
+updateCounter();  // first iteration
+
+
+// Cycle Functionality
+function mainTimerFunction() {
+  if (hovering) return;  // pause on mouse hover
+  // perform required changes
+  incrementIndeces();
+  updateCounter();
+  incrementClasses();
+}
+
+// reset timer when user stop hovering
+function resetTimer() {
+  clearInterval(mainTimer);
+  mainTimer = setInterval(mainTimerFunction, 5000);
+}
+
+// initial timer
+let mainTimer = setInterval(mainTimerFunction, 5000);
+
+
+// Button Controls
+let left_btn = document.getElementById("highlights-ctrl-left");
+let right_btn = document.getElementById("highlights-ctrl-right");
+
+function backBtn() {
+  decrementIndeces();
+  updateCounter();
+  decrementClasses();
+  resetTimer();
+}
+
+function nextBtn() {
+  incrementIndeces();
+  updateCounter();
+  incrementClasses();
+  resetTimer();
+}
+
+left_btn.onclick = () => backBtn();
+right_btn.onclick = () => nextBtn();
